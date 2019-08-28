ff153f20ff41efea533737fbddc9091900d851c7
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2019-08-28 22:01:34 +0200

    enable short-hand instantiation

--- animlib/geometies/rect.py
+++ animlib/geometies/rect.py
@@ -3,13 +3,14 @@ from animlib.utils.points import convertToPoints, interpolateLinear
 
 class Rect(Base):
     
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
+        assert len(args) == 0 or len(args) == 4, "either pass 4 arguments for x, y, w, h or no unnamed arguments"
         super().__init__(**kwargs)
 
-        self._x = 0.0
-        self._y = 0.0
-        self._w = 0.0
-        self._h = 0.0
+        self._x = args[0] if len(args) > 0 else 0.0 
+        self._y = args[1] if len(args) > 1 else 0.0
+        self._w = args[2] if len(args) > 2 else 1.0
+        self._h = args[3] if len(args) > 3 else 1.0
 
         for key in kwargs:
             arg = kwargs[key]
